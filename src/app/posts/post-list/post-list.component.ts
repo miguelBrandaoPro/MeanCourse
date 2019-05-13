603681aac2550db32d952176449ee5bdec690a06
Miguel Brandão <miguel.brandao@havasdigitalfactory.com>
2019-05-13 11:58:27 +0200

    init du projet

---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { Post } from '../post.model';
+import { PostsService } from '../posts.service';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent implements OnInit, OnDestroy {
+/*   posts = [
+    { title: 'First post', content: "This is the first post's content" },
+    { title: 'Second post', content: "This is the second post's content" },
+    { title: 'Third post', content: "This is the third post's content" }
+  ]; */
+
+  posts: Post[] = [];
+  isLoading = false;
+  private postsSub: Subscription;
+
+  constructor(public postsService: PostsService) {}
+
+  ngOnInit() {
+    this.isLoading = true;
+    this.postsService.getPosts();
+
+    this.postsSub = this.postsService.getPostUpdateListener().subscribe((posts: Post[]) => {
+      this.isLoading = false;
+      this.posts = posts;
+    });
+  }
+
+  onDelete(postId: string) {
+    this.postsService.deletePost(postId);
+  }
+
+  ngOnDestroy() {
+    this.postsSub.unsubscribe();
+  }
+}
